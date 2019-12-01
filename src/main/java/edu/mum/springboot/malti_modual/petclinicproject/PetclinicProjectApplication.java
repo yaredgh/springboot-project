779c7a786f9de6952eff7045b380cdb8a4d5308d
Yaredg <46734446+Yaredg@users.noreply.github.com>
2019-12-01 16:14:13 -0600

    initial project added

---
+++ src/main/java/edu/mum/springboot/malti_modual/petclinicproject/PetclinicProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.springboot.malti_modual.petclinicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/mum/springboot/malti_modual/petclinicproject/PetclinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.springboot.malti_modual.petclinicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
