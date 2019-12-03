f8bec5aae8a9bc39c0bb03419e4e0345a7ebc03c
Yaredg <46734446+Yaredg@users.noreply.github.com>
2019-12-02 21:16:48 -0600

    index page added

---
+++ pet-clinic-data/src/main/java/edu/mum/springboot/malti_modual/petclinicproject/Controllers/IndexController.java
@@ -0,0 +1,15 @@
+package edu.mum.springboot.malti_modual.petclinicproject.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+
+
+        return "index";
+    }
+}
